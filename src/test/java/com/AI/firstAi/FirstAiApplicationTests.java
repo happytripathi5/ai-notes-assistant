97449c28051b44d5d9626700dce1647fc16ca728
utkarsh <lionel.happy2014@gmail.com>
2026-08-17 00:57:26 +0530

    Basic setup done day 1 with Spring anthropic Ai dependency

---
+++ src/test/java/com/AI/firstAi/FirstAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AI.firstAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
